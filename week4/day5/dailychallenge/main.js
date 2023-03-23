66145a0d5e5f260d8941c105f32a2c80b2fa855b
malalatyna <raherinirina.malalatiana@gmail.com>
2023-03-23 21:15:04 +0400

    modified

--- week4/day5/dailychallenge/main.js
+++ week4/day5/dailychallenge/main.js
@@ -7,27 +7,37 @@
 // Take 1 down, pass it around
 // 98 bottles of beer on the wall
 
-
-let numBottles = parseInt(prompt("Enter number of bottles?"));
-
-let bottlesToTakeDown = 1;
-
-while (numBottles > 0) {
-  console.log(numBottles + " bottles of beer on the wall");
-  console.log(numBottles + " bottles of beer");
-  numBottles -= bottlesToTakeDown;
+function getNumBottles() {
+    return parseInt(prompt("Enter number of bottles?"));
+}
   
-  if (numBottles <= 0) {
+const numBottles = getNumBottles();
+singSong(numBottles);
+
+function singSong(numBottles) {
+    let bottlesToTakeDown = 1;
+  
+    while (numBottles > 0) {
+      console.log(numBottles + " bottles of beer on the wall");
+      console.log(numBottles + " bottles of beer");
+      numBottles -= bottlesToTakeDown;
+  
+      if (numBottles <= 0) {
+        console.log("No bottle of beer on the wall.");
+      } else if (numBottles === 1) {
+        console.log("Take " + bottlesToTakeDown + " down, pass it around");
+        console.log(numBottles + " bottle of beer on the wall");
+      } else {
+        console.log("Take " + bottlesToTakeDown + " down, pass them around");
+        console.log(numBottles + " bottles of beer on the wall");
+      }
+  
+      bottlesToTakeDown++;
+    }
+  
     console.log("No bottle of beer on the wall.");
-  } else if (numBottles === 1) {
-    console.log("Take " + bottlesToTakeDown + " down, pass it around");
-    console.log(numBottles + " bottle of beer on the wall");
-  } else {
-    console.log("Take " + bottlesToTakeDown + " down, pass them around");
-    console.log(numBottles + " bottles of beer on the wall");
   }
   
-  bottlesToTakeDown++;
-}
 
-console.log("No bottle of beer on the wall.");
+  
+  
