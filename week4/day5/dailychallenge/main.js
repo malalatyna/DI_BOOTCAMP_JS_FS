b23e53f15a3cd0254a227dcc171ca6ab4cece058
malalatyna <raherinirina.malalatiana@gmail.com>
2023-03-23 20:43:25 +0400

    changed

--- week4/day5/dailychallenge/main.js
+++ week4/day5/dailychallenge/main.js
@@ -3,20 +3,3 @@
 let numberOfCountDown=prompt("Please give a number to begin counting down bottles");
 
 let numberOfCountDownInInt=parseInt(numberOfCountDown);
-
-
-
-//   function countDown()
-//   {
-    // while(numberOfCountDownInInt<=99){
-        
-        for(let i=numberOfCountDownInInt; i>=2; i--) {
-            for (let j=1; j<=numberOfCountDownInInt;j++){
-            console.log(i,"bottles of beer on the wall");
-            console.log(i,"bottles of beer");
-            console.log("Take",j,"down, pass it around");
-        }
-    }
-//     countDown(); 
-// }           
- 
