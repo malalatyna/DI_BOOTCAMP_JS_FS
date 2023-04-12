fbc18c65bafb4070f8be1afcb8c0182e78be2358
malalatyna <raherinirina.malalatiana@gmail.com>
2023-04-12 11:30:45 +0400

    commit

---
+++ week7/day2/exercises XP/Exo 2/main.js
@@ -0,0 +1,17 @@
+//Exercise 2 : Ternary Operator
+
+// function winBattle(){
+//     return true;
+// }
+//Transform the winBattle() function to an arrow function.
+const winBattle = () => {
+    return true;
+}
+//Create a variable called experiencePoints.
+// Assign to this variable, a ternary operator. If winBattle() is true, the experiencePoints variable should be equal to 10, else the variable should be equal to 1.
+// Console.log the experiencePoints variable.
+let experiencePoints = winBattle() ? 10 : 1;
+console.log(experiencePoints);
+
+
+   
